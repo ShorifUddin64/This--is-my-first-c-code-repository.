6cabca2e89e5c2a6d13d18992021703892f3e004
MD. SHORIF UDDIN <shorifuddin64662@gmail.com>
2025-12-25 23:50:33 +0600

    Boro_Din.cpp

---
+++ Boro_Din.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    int x=5;
+    for(int i=1;i<=x;i++){
+            for(int k=40;k>i;k--)
+            {
+                cout<<" ";}
+                for(int l=1;l<i+1;l++){
+
+                cout<<"*";}
+                for(int j=0;j<i;j++){
+
+        cout<<"*";
+        }
+    cout<<endl;}
+    for(int u=0;u<2;u++){
+                for(int y=38;y>0;y--){
+        cout<<" ";}
+
+    cout<<"||||"<<endl;}
+    for(int y=32;y>0;y--){
+            cout<<" ";}
+        cout<<"Merry Christmas ";
+
+getch ();
+}
+
